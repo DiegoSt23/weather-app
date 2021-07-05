52cc212af7c93d4df7ac810e182bc66e7e4149f0
diego alvarez <diego.stonerough@gmail.com>
2021-07-04 19:39:52 -0500

    se removio spinner

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
-import React from 'react';
-import { Suspense } from 'react';
 import './App.css';
-const WeatherBox = React.lazy(() => import("./Weather-box"))
+import WeatherBox from "./Weather-box"
 
 function App() {
   return (
     <div>
       <header className="App-header">
-        <h1>Weather App</h1>
-        <Suspense fallback={<div className="spinner">Loading...</div>}>
-          <WeatherBox/>
-        </Suspense>
+        <h1>Weather App</h1>        
+        <WeatherBox/>        
       </header>
     </div>
   );
